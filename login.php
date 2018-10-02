b0cc71da7e7b155d62abac5b216a489003ae37af
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-10-02 11:10:55 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,17 @@
 <?php
-
-
-
-
+    session_start();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form action="prosesLogin.php" method="post">
+        Username : <input type="text" name="username"><br><br>
+        Password : <input type="password" name="password"><br>
+        <br>
+        <input type="submit" name="kirim" value="LOGIN">
+    </form>
+</body>
+</html>
