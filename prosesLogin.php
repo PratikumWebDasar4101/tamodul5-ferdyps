e2cc5a3331df5372e54abe0124f5a050ff5adc73
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-10-02 11:11:18 +0700

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,40 @@
-<?php 
+<?php
+session_start();
+$akun = array(
+    array("ferdy","ferdy"),
+    array("samsul",1234)
+);  
+if (@$_SESSION['sukses']) {
+    header("location: registrasi.php");
+} 
 
+if (@$_GET['logout']) {
+    session_destroy();
+    header("location: login.php");
+}
+?>
+<?php
+if (isset($_POST['username'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-
-
-
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+   
+    $cek=0;
+    for ($i = 0; $i < count($akun); $i++) { 
+        if ( $akun[$i][0] == $username && $akun[$i][1] == $password) {
+            $_SESSION['sukses']="mantul";
+            header("Location: registrasi.php");
+            $cek=1;
+        }
+    }
+    if ($cek == 0) {
+        ?>
+        <script>
+        alert("Username/Password salah");
+        </script>
+        <?php
+    }
+}
 ?>
